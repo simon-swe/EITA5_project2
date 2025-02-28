78b40cbb5d85db61bb0ccc106e37312dd08e15b7
Julius Lam <julius.lam2@gmail.com>
2025-02-28 14:17:48 +0100

    rename

--- logger.java
+++
@@ -1,9 +0,0 @@
-public class logger {
-/*
- * Keeps a audit log on everything that happens with the records in the server and who accessed them
- * The log will be saved in a file
- * The log will contain the username of the user that accessed the file, a timestamp and what action was taken
- */
-
- 
-}
