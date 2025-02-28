2d81c32b9f610c7be7335180e4008b71dfe5f1f9
Julius Lam <julius.lam2@gmail.com>
2025-02-28 15:44:36 +0100

    Add user test

--- LoggerTest.java
+++ LoggerTest.java
@@ -1,9 +1,14 @@
 public class LoggerTest {
     public static void main(String[] args) {
-        Logger l = new Logger();
+        Logger l = Logger.getInstance();
         l.log("Julius Lam", "Doctor", "Added new medicine to patient");
         l.log("Simon Swedenborg", "Government", "Read patient record");
-        l.closeLog();
+
+        user user1  = new user("Michael Jacksson", "123", "Patient", "Heart");
+        user1.readFiles();
+
+
+    
     }
    
 }
