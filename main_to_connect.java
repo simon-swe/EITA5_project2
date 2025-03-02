10a76435aa83082f0a61587890ce2f16fe64f22d
Aitchi <hannavallin13@gmail.com>
2025-03-02 16:52:12 +0100

    New commit

--- main_to_connect.java
+++ main_to_connect.java
@@ -1,14 +1,47 @@
+import java.util.Scanner;
+
 public class main_to_connect {
 
     public static void main(String[] args) {
-        Login login = new Login();
-        user user = new user();
-        Records records = new Records();
+        login login = new login();
         if (login.authenticate()) {
             System.out.println("Login successful!");
 
         } else {
             System.out.println("Wrong username or password");
+        }
+        user user = new user(login.returnUser(), login.returnType(), login.returnDepartment());
+        Records records = new Records(user);
+        Scanner input = new Scanner(System.in);
+        System.out.println("What would you like to do?");
+        System.out.println("1. Get a record");
+        System.out.println("2. Create a record");
+        System.out.println("3. Delete a record");
+
+        int choice = input.nextInt();
+        if(choice == 1){
+            System.out.println("Enter the patient name: ");
+            String patient = input.next();
+            records.getRecord(patient);
+        }
+        else if (choice == 2){
+            System.out.println("Enter the patient name: ");
+            String patient = input.next();
+            System.out.println("Enter the doctor name: ");
+            String doctor = input.next();
+            System.out.println("Enter the nurse name: ");
+            String nurse = input.next();
+            System.out.println("Enter the department: ");
+            String department = input.next();
+            records.createRecord(patient, doctor, nurse, department);
+        }
+        else if (choice == 3){
+            System.out.println("Enter the patient name: ");
+            String patient = input.next();
+            records.deleteRecord(patient);
+        }
+        else{
+            System.out.println("Invalid choice");
         }
     }
 }
