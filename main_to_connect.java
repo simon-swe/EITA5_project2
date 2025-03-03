3007c386f34c55b96a38417585763d992f25c564
Julius Lam <julius.lam2@gmail.com>
2025-03-03 16:06:30 +0100

    Add printwriter usage

--- main_to_connect.java
+++ main_to_connect.java
@@ -16,39 +16,50 @@ public class main_to_connect {
     }
 
     public void program() {
-        System.out.println("What would you like to do?");
-        System.out.println("1. Get a record");
-        System.out.println("2. Create a record");
-        System.out.println("3. Delete a record");
+        out.println("What would you like to do?");
+        out.flush();
+        out.println("1. Get a record");
+        out.flush();
+        out.println("2. Create a record");
+        out.flush();
+        out.println("3. Delete a record");
+        out.flush();
         try {
             String c = in.readLine();
             int choice = Integer.parseInt(c);
             if(choice == 1){
-                System.out.println("Enter the patient name: ");
+                out.println("Enter the patient name: ");
                 String patient = in.readLine();
                 records.getRecord(patient);
             }
             else if (choice == 2){
-                System.out.println("Enter the patient name: ");
+                out.println("Enter the patient name: ");
+                out.flush();
                 String patient = in.readLine();
-                System.out.println("Enter the doctor name: ");
+                out.println("Enter the doctor name: ");
+                out.flush();
                 String doctor = in.readLine();
-                System.out.println("Enter the nurse name: ");
+                out.println("Enter the nurse name: ");
+                out.flush();
                 String nurse = in.readLine();
-                System.out.println("Enter the department: ");
+                out.println("Enter the department: ");
+                out.flush();
                 String department = in.readLine();
                 records.createRecord(patient, doctor, nurse, department);
             }
             else if (choice == 3){
-                System.out.println("Enter the patient name: ");
+                out.println("Enter the patient name: ");
+                out.flush();
                 String patient = in.readLine();
                 records.deleteRecord(patient);
             }
             else{
-                System.out.println("Invalid choice");
+                out.println("Invalid choice");
+                out.flush();
             }
         } catch (Exception e) {
             System.out.println("Exception while entering String");
+            e.printStackTrace();
         }
     }
 
